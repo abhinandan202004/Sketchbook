3344263dbaa60bb9652200c20c461f41cfd8bbad
abhinandan202004 <126962453+abhinandan202004@users.noreply.github.com>
2024-08-08 15:08:24 +0530

    Rows and columns made using DOM

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    // Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
 };
 
-makeRows(16, 16);
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
